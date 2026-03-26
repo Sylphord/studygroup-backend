056e91d000e75a105f0f6335f2a6d6fd84685a86
Sylphord <uchennatobenna201@gmail.com>
2026-03-26 21:15:31 +0100

    fix env variable names

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Admin1234')
-MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-MYSQL_DB = os.environ.get('MYSQL_DB', 'studygroup_db')
+MYSQL_USER = os.environ.get('MYSQLUSER', 'root')
+MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD', 'Admin1234')
+MYSQL_HOST = os.environ.get('MYSQLHOST', 'localhost')
+MYSQL_DB = os.environ.get('MYSQLDATABASE', 'studygroup_db')
+MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret-key-change-this')
 
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
